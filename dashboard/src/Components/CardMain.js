a356fb2fec27d5048ddfcd0d92bb73b2e13dc198
Aemal <aemallodhi@gmail.com>
2022-04-04 11:25:27 +0500

    update aemal

--- dashboard/src/Components/CardMain.js
+++ dashboard/src/Components/CardMain.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import React from "react";
+import { BsHeartFill } from "react-icons/bs";
 
 function CardMain({imgSrc, title, hearts}) {
   return (
     <div className="card_main">
-        <img src="" alt="" />
+        <img src={imgSrc} alt="" className="card_main_img" />
 
         <div className="card_main_name">
             <h2>{title}</h2>
 
             <div className="card_icon">
-                <i>  </i>
+                <i> 
+                  <BsHeartFill/> <span>{hearts}</span> 
+                  </i>
             </div>
         </div>
+
+        <div className="stats">
+          <div>
+           <p>
+             Current Bid <span>1.2 ETH</span>
+             </p>
+             </div>
+             <div>
+           <p>
+             Ending In <span>1d:12h:10m</span>
+             </p>
+             </div>
+        </div>
+
+        <div className="card_button">
+          <a href="#" className="button1">
+            Place a Bid
+            </a>
+          <a href="#" className="button2">
+            History
+            </a>
+        </div>
     </div>
-  )
+  );
 }
 
-export default CardMain
+export default CardMain;
