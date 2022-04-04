bc0fae46c41d4a8e68c021ba68e7e60ed4f66e65
Aemal <aemallodhi@gmail.com>
2022-04-04 15:29:10 +0500

    update aemal

--- dashboard/src/Components/MainRightBottom.js
+++ dashboard/src/Components/MainRightBottom.js
@@ -7,6 +7,16 @@ function MainRightBottom() {
             <h2>Top Seller</h2>
             <a href="#">View More</a>
         </div>
+
+        <div className="topSeller">
+          <div className="topSellerImg"></div>
+
+          <p className="topSellerName">Sample Name <span>@sample</span></p>
+
+          <a href="#" className="button1 btn">
+            Follow
+          </a>
+        </div>
     </div>
   );
 }
---
+++ dashboard/src/Components/TopSeller.js
@@ -0,0 +1,5 @@
+import Seller1 from "../img/seller1.jpg";
+import Seller2 from "../img/seller2.jpg";
+import Seller3 from "../img/seller3.jpg";
+import Seller4 from "../img/seller4.jpg";
+import Seller5 from "../img/seller5.jpg";
