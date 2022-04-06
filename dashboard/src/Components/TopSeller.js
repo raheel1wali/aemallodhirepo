227fc7937be9eef53c868909f973a6b921729229
Aemal <aemallodhi@gmail.com>
2022-04-06 20:16:35 +0500

    update aemal

--- dashboard/src/Components/TopSeller.js
+++ dashboard/src/Components/TopSeller.js
@@ -6,10 +6,10 @@ import Seller5 from "../img/seller5.jpg";
 
 const TopSeller = [
     {id : 1, seller_name : 'Jane Cooper', username : '@jane', imgSrc : Seller1},
-    {id : 2, seller_name : 'Ariene Maccoy', username : '@ariene', imgSrc : Seller2},
-    {id : 3, seller_name : 'Theresa Webb', username : '@theresa', imgSrc : Seller3},
-    {id : 4, seller_name : 'Raplh Edwards', username : '@raplh', imgSrc : Seller4},
-    {id : 5, seller_name : 'James Bond', username : '@james', imgSrc : Seller5}   
+    {id : 2, seller_name : 'James Bond', username : '@james', imgSrc : Seller2},
+    {id : 3, seller_name : 'Raplh Edwards', username : '@raplh', imgSrc : Seller3},
+    {id : 4, seller_name : 'Ariene Maccoy', username : '@ariene', imgSrc : Seller4},
+    {id : 5, seller_name : 'Theresa Webb', username : '@theresa', imgSrc : Seller5}   
 ];
 
 export default TopSeller;
